01923909a88abc4c31fb0976bb9adccc2af2c173
Glen Gu <guxi99@gmail.com>
2015-06-01 18:16:58 +0800

    add a new script

---
+++ updateAllWikis.php
@@ -0,0 +1,17 @@
+<?php
+require_once ('/var/www/html/Confidential.php');
+$servername = Confidential::$servername;
+$username = Confidential::$username;
+$pwd = Confidential::$pwd;
+$link=mysql_connect("$servername","$username","$pwd");
+mysql_query("SET NAMES UTF8");
+mysql_select_db("huiji",$link);
+$sql = "select domain_prefix from domain";
+$query = mysql_query($sql);
+while ($res = mysql_fetch_assoc( $query )) {
+	$arr[] = $res;
+}
+foreach($arr as $val){
+	$command = 'php update.php --conf=/var/www/virtual/ '.$val['domain_prefix'].'/LocalSettings.php --quick --doshared';
+	exec($command);
+}
