1a991c7afb00bcb892754021a8c604790c99966a
reasno <guxi99@gmail.com>
2015-07-13 11:29:22 +0800

    Hacked

--- updateAllWikis.php
+++ updateAllWikis.php
@@ -12,8 +12,14 @@ while ($res = mysql_fetch_assoc( $query )) {
 	$arr[] = $res;
 }
 foreach($arr as $val){
-	$command = 'php ./update.php --conf=/var/www/virtual/'.$val['domain_prefix'].'/LocalSettings.php --quick --doshared';
+	$conf = '/var/www/virtual/'.$val['domain_prefix'].'/LocalSettings.php';
+	$command = 'php ./update.php --conf='.$conf.' --quick --doshared';
 	exec($command);
 	$command2 = 'ln -s /var/www/src/* /var/www/virtual/'.$val['domain_prefix'].'/';
 	exec($command2);
+	$command3 = "php ../extensions/CirrusSearch/maintenance/updateSearchIndexConfig.php --conf=".$conf;
+	exec($command3);
+	$command4 = "php ../extensions/CirrusSearch/maintenance/forceSearchIndex.php --skipLinks --indexOnSkip --conf=".$conf;
+	exec($command4);
+	$command5 = "php ../extensions/CirrusSearch/maintenance/forceSearchIndex.php --skipParse --conf=".$conf; 
 }
