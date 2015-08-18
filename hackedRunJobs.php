f15b81258cb08dab3bccd60f70054fde8a44966a
Xi Gu <Glen@Xis-Mac-mini.local>
2015-08-18 14:01:29 +0800

    add hacked Runjobs

---
+++ hackedRunJobs.php
@@ -0,0 +1,19 @@
+<?php
+require_once ('/var/www/html/Confidential.php');
+$servername = Confidential::$servername;
+$username = Confidential::$username;
+$pwd = Confidential::$pwd;
+$link=mysql_connect("$servername","$username","$pwd");
+mysql_query("SET NAMES UTF8");
+mysql_select_db("huiji",$link);
+$sql = "select domain_prefix from domain";
+$query = mysql_query($sql);
+while ($res = mysql_fetch_assoc( $query )) {
+	$arr[] = $res;
+}
+
+foreach($arr as $val){
+	$conf = '/var/www/virtual/'.$val['domain_prefix'].'/LocalSettings.php';
+	$command = 'php ./runJobs.php --conf='.$conf;
+	exec($command);
+}	
